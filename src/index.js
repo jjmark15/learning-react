eac32b93b02dab0ad42ae2d0e2be5dc3bc28953e
Josh Jones <ohblonddev@gmail.com>
2019-01-28 20:44:00 +0000

    render something to page

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // create a React component
+const App = () => {
+  return <div>Hi there!</div>
+}
 
 // Show the react component on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
